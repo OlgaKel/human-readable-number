e2541ece4845a81dd8452212da51286c6af79d07
OlgaKel <nikkelos@ya.ru>
2024-07-31 10:25:40 +0700

    feat: add array

--- src/index.js
+++ src/index.js
@@ -23,44 +23,24 @@ module.exports = function toReadable (number) {
         19: 'nineteen',
     };
     if (20 <= number) {
-        tens = {
-            20: 'twenty',
-            30: 'thirty',
-            40: 'forty',
-            50: 'fifty',
-            60: 'sixty',
-            70: 'seventy',
-            80: 'eighty',
-            90: 'ninety',
-        };
+        tens = [
+            '','',
+            'twenty',
+            'thirty',
+            'forty',
+            'fifty',
+            'sixty',
+            'seventy',
+            'eighty',
+            'ninety',
+        ];
         const num = String(number).split('');
         if (num.length === 2)
             if (number % 10 == 0) {
-                return tens[number];
+                return tens[num[0]];
             } else {
-                if (num[0] == 2) {
-                    return 'twenty' + ' ' + nums[num[1]];
-                }
-                if (num[0] == 3) {
-                    return 'thirty' + ' ' + nums[num[1]];
-                }
-                if (num[0] == 4) {
-                    return 'forty' + ' ' + nums[num[1]];
-                }
-                if (num[0] == 5) {
-                    return 'fifty' + ' ' + nums[num[1]];
-                }
-                if (num[0] == 6) {
-                    return 'sixty' + ' ' + nums[num[1]];
-                }
-                if (num[0] == 7) {
-                    return 'seventy' + ' ' + nums[num[1]];
-                }
-                if (num[0] == 8) {
-                    return 'eighty' + ' ' + nums[num[1]];
-                }
-                if (num[0] == 9) {
-                    return 'ninety' + ' ' + nums[num[1]];
+                if (num[0] >= 2) {
+                    return tens[num[0]] + ' ' + nums[num[1]];
                 }
             }
         if (num.length === 3)
@@ -68,29 +48,14 @@ module.exports = function toReadable (number) {
                 if (number % 10 === 0)
                     if (num[1] != 2) {
                         if (num[1] != 3) {
-                            if (num[1] == 4) {
-                                return nums[parseInt(num[0])] + ' hundred ' + tens[40];
-                            }
-                            if (num[1] == 5) {
-                                return nums[parseInt(num[0])] + ' hundred ' + tens[50];
-                            }
-                            if (num[1] == 6) {
-                                return nums[parseInt(num[0])] + ' hundred ' + tens[60];
-                            }
-                            if (num[1] == 7) {
-                                return nums[parseInt(num[0])] + ' hundred ' + tens[70];
-                            }
-                            if (num[1] == 8) {
-                                return nums[parseInt(num[0])] + ' hundred ' + tens[80];
-                            }
-                            if (num[1] == 9) {
-                                return nums[parseInt(num[0])] + ' hundred ' + tens[90];
+                            if (num[1] >= 4) {
+                                return nums[parseInt(num[0])] + ' hundred ' + tens[num[1]];
                             }
                         } else {
-                            return nums[parseInt(num[0])] + ' hundred ' + tens[30];
+                            return nums[parseInt(num[0])] + ' hundred ' + tens[3];
                         }
                     } else {
-                        return nums[parseInt(num[0])] + ' hundred ' + tens[20];
+                        return nums[parseInt(num[0])] + ' hundred ' + tens[2];
                     }
                 if (num[1] == 0) {
                     return nums[parseInt(num[0])] + ' hundred ' + nums[num[2]];
@@ -127,29 +92,8 @@ module.exports = function toReadable (number) {
                         return nums[parseInt(num[0])] + ' hundred ' + nums[19];
                     }
                 }
-                if (num[1] == 2) {
-                    return nums[parseInt(num[0])] + ' hundred ' + tens[20] + ' ' + nums[num[2]];
-                }
-                if (num[1] == 3) {
-                    return nums[parseInt(num[0])] + ' hundred ' + tens[30] + ' ' + nums[num[2]];
-                }
-                if (num[1] == 4) {
-                    return nums[parseInt(num[0])] + ' hundred ' + tens[40] + ' ' + nums[num[2]];
-                }
-                if (num[1] == 5) {
-                    return nums[parseInt(num[0])] + ' hundred ' + tens[50] + ' ' + nums[num[2]];
-                }
-                if (num[1] == 6) {
-                    return nums[parseInt(num[0])] + ' hundred ' + tens[60] + ' ' + nums[num[2]];
-                }
-                if (num[1] == 7) {
-                    return nums[parseInt(num[0])] + ' hundred ' + tens[70] + ' ' + nums[num[2]];
-                }
-                if (num[1] == 8) {
-                    return nums[parseInt(num[0])] + ' hundred ' + tens[80] + ' ' + nums[num[2]];
-                }
-                if (num[1] == 9) {
-                    return nums[parseInt(num[0])] + ' hundred ' + tens[90] + ' ' + nums[num[2]];
+                if (num[1] >= 2) {
+                    return nums[parseInt(num[0])] + ' hundred ' + tens[num[1]] + ' ' + nums[num[2]];
                 }
             } else {
                 return nums[parseInt(num[0])] + ' hundred';
